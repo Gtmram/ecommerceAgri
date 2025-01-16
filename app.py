b12b81629e460add3b56c4a5b642e8614624f597
xplorerh <080bei010@ioepc.edu.np>
2025-01-16 22:34:38 +0545

    file upload last of 3 magh

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
-from flask import Flask
+from flask import Flask,render_template,redirect
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, World! Welcome to my first Flask app!"
+def Home():
+    return render_template("frontpage.html",title="Home")
+@app.route('/features')
+def features():
+    return render_template("frontpage.html",title="features")
+@app.route('/research_data')
+def research_data():
+    return render_template("frontpage.html",title="research_data")
+@app.route('/student_zone')
+def student_zone():
+    return render_template("frontpage.html",title="student_zone")
+@app.route('/contact')
+def contact():
+    return render_template("frontpage.html",title="contact")
 
+@app.route('/marketplace')
+def marketplace():
+    return render_template("frontpage.html",title="marketplace")
 if __name__ == '__main__':
     app.run(debug=True)
