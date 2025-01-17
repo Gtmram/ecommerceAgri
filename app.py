3db190d7bbbbf22550e527d9b86fb880552374aa
xplorerh <080bei010@ioepc.edu.np>
2025-01-17 16:07:37 +0545

    vegitable card

--- app.py
+++ app.py
@@ -1,22 +1,28 @@
 from flask import Flask,render_template,redirect
 
+
 app = Flask(__name__)
 
 @app.route('/')
 def Home():
     return render_template("frontpage.html",title="Home")
+
 @app.route('/features')
 def features():
     return render_template("frontpage.html",title="features")
+
 @app.route('/research_data')
 def research_data():
     return render_template("frontpage.html",title="research_data")
+
 @app.route('/student_zone')
 def student_zone():
     return render_template("frontpage.html",title="student_zone")
+
 @app.route('/contact')
 def contact():
     return render_template("frontpage.html",title="contact")
+
 
 @app.route('/marketplace')
 def marketplace():
